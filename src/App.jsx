5b20914971aa178bd0decdd759c1cbe1ee1f059e
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2026-04-20 21:49:04 +0200

    Migrate app entry to React Vite while preserving legacy UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import { ExperienceStateProvider, useExperienceState } from './features/experience/state/experienceState.jsx';
-import { useAppPresenter } from './app/presenter/useAppPresenter';
-import AppView from './app/view/AppView';
-
-function AppContent() {
-  const { state, dispatch } = useExperienceState();
-  const { onViewChange, onEnterExperience } = useAppPresenter({ dispatch });
-
-  return <AppView currentView={state.currentView} onEnterExperience={onEnterExperience} onViewChange={onViewChange} />;
-}
+import LegacyApp from './LegacyApp';
 
 function App() {
-  return (
-    <ExperienceStateProvider>
-      <AppContent />
-    </ExperienceStateProvider>
-  );
+  return <LegacyApp />;
 }
 
 export default App;
