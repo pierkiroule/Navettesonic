7d16c1116f0bd55a5c4719e4396d2e478338f176
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2026-04-30 09:49:09 +0200

    refactor(legacy): split shell into focused React components

---
+++ src/features/legacy/components/LegacyRuntimeBootstrap.jsx
@@ -0,0 +1,8 @@
+import { useLegacyBootstrap } from '../hooks/useLegacyBootstrap';
+
+function LegacyRuntimeBootstrap() {
+  useLegacyBootstrap();
+  return null;
+}
+
+export default LegacyRuntimeBootstrap;
