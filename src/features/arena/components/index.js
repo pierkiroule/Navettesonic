d9034e3a757a3ab45f3f578d227d0ca60c819513
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2026-05-02 09:01:48 +0200

    Add arena UI component stubs and exports

---
+++ src/features/arena/components/index.js
@@ -0,0 +1,11 @@
+export { ArenaPublishPanel } from './ArenaPublishPanel.jsx';
+export { FishController } from './FishController.jsx';
+export {
+  ModeBadge,
+  ArenaStatusChip,
+  PublishButton,
+  CopyLinkButton,
+  VisitorHintOverlay,
+  ErrorStateCard,
+  LoadingState,
+} from './ArenaUiComponents.jsx';
