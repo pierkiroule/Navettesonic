c94f8b270213ab5a7da180939ba15ef44486d929
Garnier Pierre-Henri <39243998+pierkiroule@users.noreply.github.com>
2026-05-16 13:07:46 +0200

    Fix missing useRef import in SoonApp

--- src/pages/SoonApp.jsx
+++ src/pages/SoonApp.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import SidePanel from "../components/SidePanel.jsx";
 import SoonCanvas from "../components/SoonCanvas.jsx";
 import WorkflowShell from "../components/WorkflowShell.jsx";
